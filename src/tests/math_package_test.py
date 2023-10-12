569dafc18f52864022295db10a30d000d2efba03
Sebastian-deKieviet <sebastian.dekieviet@projectbinder.eu>
2023-10-12 09:11:49 +0200

    test

--- src/tests/math_package_test.py
+++ src/tests/math_package_test.py
@@ -3,14 +3,14 @@ import pytest
 from pipeline_test.math_package import add_function, subtract_function
 
 class TestClass:
-    def one_plus_one_is_three(self):
+    def test_one_plus_one_is_three(self):
         assert add_function(1, 2) == 3
 
-    def two_plus_two_is_four(self):
+    def test_two_plus_two_is_four(self):
         assert add_function(2, 2) == 4
 
-    def one_minus_two_is_negative_one(self):
+    def test_one_minus_two_is_negative_one(self):
         assert subtract_function(1, 2) == -1
 
-    def two_minus_two_is_zero(self):
+    def test_two_minus_two_is_zero(self):
         assert subtract_function(2, 2) == 0
